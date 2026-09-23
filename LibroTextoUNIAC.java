264125ece2ed0f05ccf9847075871c0fbf8bed75
sebastindiaz123-code <sebastindiaz123@gmail.com>
2026-09-23 04:30:48 -0500

    actualizacion al libro texto uniac.java ya que tenia ciertos parametros en el error al ejecutar

--- LibroTextoUNIAC.java
+++ LibroTextoUNIAC.java
@@ -1,16 +1,31 @@
 public class LibroTextoUNIAC extends LibroTexto {
+
     private String facultad;
 
-    public LibroTextoUNIAC(String titulo, String autor, int numEjemplares, int numPrestados, String curso, String facultad) {
-        super(titulo, autor, numEjemplares, numPrestados, curso);
+    public LibroTextoUNIAC() {
+        super();
+        this.facultad = "";
+    }
+
+    public LibroTextoUNIAC(String titulo, String autor, int numEjemplares,
+                            int numEjemplaresPrestados, String curso, String facultad) {
+        super(titulo, autor, numEjemplares, numEjemplaresPrestados, curso);
         this.facultad = facultad;
     }
 
-    public String getFacultad() { return facultad; }
-    public void setFacultad(String facultad) { this.facultad = facultad; }
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
 
     @Override
     public String toString() {
-        return super.toString() + " - Facultad: " + facultad;
+        return "LibroTextoUNIAC{" +
+                super.toString() +
+                ", facultad='" + facultad + '\'' +
+                '}';
     }
 }
