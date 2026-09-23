b7bf44a315537235f18a5d99032bd8e90b3eebd4
Adrian92-lab <adrianandrade@estudiante.uniajc.edu.co>
2026-09-23 03:38:18 -0500

    Add files via upload

---
+++ LibroTexto.java
@@ -0,0 +1,16 @@
+public class LibroTexto extends Libro {
+    private String curso;
+
+    public LibroTexto(String titulo, String autor, int numEjemplares, int numPrestados, String curso) {
+        super(titulo, autor, numEjemplares, numPrestados);
+        this.curso = curso;
+    }
+
+    public String getCurso() { return curso; }
+    public void setCurso(String curso) { this.curso = curso; }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - Curso: " + curso;
+    }
+}
