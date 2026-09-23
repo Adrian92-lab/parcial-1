0b79bbfc0cce8bf0de376c2aab90127e2c137927
sebastindiaz123-code <sebastindiaz123@gmail.com>
2026-09-23 03:10:48 -0500

    Add files via upload
    se volvieron a agrega rlos archivos

---
+++ src/main/java/LibroTexto.java
@@ -0,0 +1,31 @@
+public class LibroTexto extends Libro {
+
+    private String curso;
+
+    public LibroTexto() {
+        super();
+        this.curso = "";
+    }
+
+    public LibroTexto(String titulo, String autor, int numEjemplares,
+                       int numEjemplaresPrestados, String curso) {
+        super(titulo, autor, numEjemplares, numEjemplaresPrestados);
+        this.curso = curso;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    @Override
+    public String toString() {
+        return "LibroTexto{" +
+                super.toString() +
+                ", curso='" + curso + '\'' +
+                '}';
+    }
+}
